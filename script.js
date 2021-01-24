df7e55112f6107132e2609c396f7a277c19db43d
Kartik <kartikguptahari@gmail.com>
2021-01-24 23:04:10 +0530

    letsgo

---
+++ script.js
@@ -0,0 +1,30 @@
+const btns = document.querySelectorAll('.btn');
+const screen = document.querySelector('.screen');
+const equalbtn = document.querySelector('.btn-equal');
+const clearbtn = document.querySelector('.btn-clear');
+
+for(let i = 0; i < btns.length; i++){
+     btns[i].addEventListener('click',function(){
+         let number = btns[i].getAttribute('data-num');
+         screen.value += number
+     })
+}
+
+
+
+equalbtn.addEventListener('click',function(){
+    if(screen.value === ''){
+        alert('Input Is Empty')
+    }
+    else{
+        let value = eval(screen.value);
+        screen.value = value;
+    }
+   
+})
+
+
+
+clearbtn.addEventListener('click',function(){
+  screen.value = '';
+})
